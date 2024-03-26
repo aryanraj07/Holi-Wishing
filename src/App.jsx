2175291870d00d90c38c99cc03e5aa390ff9789b
aryanraj07 <aryanraj010010@gmail.com>
2024-03-26 14:09:22 +0530

    adding all react file of wishing holi

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleClick = () => {
+    setShowMessage(true);
+  };
+
+  return (
+    <div className="App">
+      <div className="card" onClick={handleClick}>
+        Click me!
+      </div>
+      {showMessage && (
+        <div className="message">
+          <p className="message-text">
+            Happy Holi, Shreya! Sending colorful wishes to you, uncle, aunty,dii
+            and your whole family!
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
